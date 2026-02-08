cfbbe36d4975af65db52e637b3bd09eadb677d48
Vaishnavi778 <goswamivaishnavi11@gmail.com>
2026-02-08 22:01:33 +0530

    Updated API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000"
+  baseURL: "https://hrm-system-1-klrq.onrender.com",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default API;
